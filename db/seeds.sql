1e402cc7d77b8cebdcad16f235356a6c24bf53b6
Lord Evan <114256148+LordEvanElias@users.noreply.github.com>
2023-01-22 15:37:50 -0600

    Added the supposed connection to the sql database. Doesn't work.

--- db/seeds.sql
+++ db/seeds.sql
@@ -12,10 +12,10 @@ INSERT INTO role
     (title, salary, department_id)
 VALUES
     ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
+    ('Salesperson', 45000, 1),
     ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
+    ('Software Engineer', 80000, 2),
+    ('Account Manager', 100000, 3),
     ('Accountant', 125000, 3),
     ('Legal Team Lead', 250000, 4),
     ('Lawyer', 190000, 4);
@@ -23,11 +23,11 @@ VALUES
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
+    ('Aaron', 'Rogers', 1, NULL),
+    ('Betty', 'White', 2, 1),
+    ('Michael', 'Jackson', 3, NULL),
+    ('Derek', 'Jeter', 4, 3),
+    ('Yukina', 'Yamaguchi', 5, NULL),
     ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
+    ('Antonio', 'Bianchi', 7, NULL),
     ('Tom', 'Allen', 8, 7);
--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
 
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "root",
+    database: "tracker",
+  },
 
-function viewDepartments(){
-  console.log("Viewing Departments")
+  console.log(`Connected to the tracker database.`)
+);
+
+function viewDepartments() {
+  db.query("SELECT * FROM department", function (err, results) {
+    console.table(results);
+  });
+  console.log("Success!");
 }
-
-
 
 inquirer
   .prompt([
     {
-      type: 'list',
-      message: 'What would you like to do?',
-      name: 'choice',
-      choices:[{name:'View all departments', value:"VIEW_DEPARTMENTS"}]
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        { name: "View all departments", value: "VIEW_DEPARTMENTS" },
+        { name: "View all roles", value: "VIEW_ROLES" },
+      ],
     },
-  ]);
-  
-  .then((response) =>
-  console.log(response.choice)
+  ])
 
-  if (response.choice === "VIEW_DEPARTMENTS"){
-    viewDepartments()
-  }
-  );
+  .then((response) => {
+    console.log(response.choice);
+
+    if (response.choice === "VIEW_DEPARTMENTS") {
+      viewDepartments();
+    }
+  });
